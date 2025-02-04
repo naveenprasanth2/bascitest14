f59ace2eb659bca8f2d26f0ac0261bd80ddbd3ff
naveen prasanth <naveenprasanth2@gmail.com>
2025-02-04 19:58:12 +0530

    effective WaterContainer solution

--- src/main/java/org/example/dsa/WaterContainer.java
+++ src/main/java/org/example/dsa/WaterContainer.java
@@ -2,17 +2,20 @@ package org.example.dsa;
 
 public class WaterContainer {
     public static void main(String[] args) {
-        int[] height = {1, 8, 6, 2, 5, 4, 8, 100, 7}; // Example input array
+        int[] height = {1, 8, 99, 6, 2, 5, 4, 8, 100, 7}; // Example input array
         int max = 0;
+        int left = 0;
+        int right = (height.length - 1);
 
-        for (int i = 0; i < height.length; i++) {
-            int initial = height[i];
-            for (int j = i + 1; j < height.length; j++) {
-                int minValue = Math.min(initial, height[j]);
-                int product = (minValue * (j - i));
-                if (product > max){
-                    max = product;
-                }
+        while (left < right) {
+            int product = Math.min(height[right], height[left]);
+            int dis = right - left;
+            int finalProduct = product * dis;
+            max = Math.max(max, finalProduct);
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
             }
         }
 
