f770e856aad9c39412a6434938a38a7aa0af1e3f
naveen prasanth <naveenprasanth2@gmail.com>
2025-02-09 18:23:55 +0530

    longest palindrome string logic practised

---
+++ src/main/java/org/example/chatgpt/dsa/LongestPalindrome1.java
@@ -0,0 +1,33 @@
+package org.example.chatgpt.dsa;
+
+public class LongestPalindrome1 {
+    static int resultIndex;
+    static int resultLength;
+
+    public static void main(String[] args) {
+        String s = "racecar";
+        for (int start = 0; start < s.length(); start++) {
+            expandRange(s, start, start);
+            expandRange(s, start, start + 1);
+        }
+        System.out.println(s.substring(resultIndex, resultIndex + resultLength));
+    }
+
+    private static void expandRange(String s, int begin, int end) {
+        if (s.length() <= 1) {
+            resultIndex = 0;
+            resultLength = s.length();
+        }
+
+        while (begin >= 0 && end < s.length() && s.charAt(begin) == s.charAt(end)) {
+            begin--;
+            end++;
+        }
+
+        if (resultLength < (end - 1) - (begin + 1) + 1) {
+            resultIndex = begin + 1;
+            resultLength = (end - 1) - (begin + 1) + 1;
+        }
+
+    }
+}
