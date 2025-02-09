ad8faee94f6d487b53c1a435b451b157a18745ad
naveen prasanth <naveenprasanth2@gmail.com>
2025-02-09 18:25:51 +0530

    added thread local eval code changes

--- src/main/java/org/example/tesco/threading/ThreadLocalEval.java
+++ src/main/java/org/example/tesco/threading/ThreadLocalEval.java
@@ -1,19 +1,19 @@
 import org.example.tesco.threading.User;
 
-public static class ThreadLocalEval {
+public static class ThreadLocalEval1 {
     public static ThreadLocal<User> threadLocal = new ThreadLocal<>();
 }
 
 static class Service1 {
     void process() {
         User user = new User(1);
-        ThreadLocalEval.threadLocal.set(user);
+        ThreadLocalEval1.threadLocal.set(user);
     }
 }
 
 static class Service2 {
     void process() {
-        User user = ThreadLocalEval.threadLocal.get();
+        User user = ThreadLocalEval1.threadLocal.get();
         System.out.println(user);
     }
 }
