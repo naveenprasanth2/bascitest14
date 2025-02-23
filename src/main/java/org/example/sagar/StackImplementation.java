10f5c643a7e44f6be1e222663268362021353834
naveen prasanth <naveenprasanth2@gmail.com>
2025-02-23 16:39:35 +0530

    stack implementation using array

--- src/main/java/org/example/sagar/StackImplementation.java
+++ src/main/java/org/example/sagar/StackImplementation.java
@@ -1,9 +1,15 @@
 package org.example.sagar;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 public class StackImplementation {
-    int size = 1;
-    int[] a = new int[size];
+    int[] a;
     int pointer = 0;
+
+    public StackImplementation(int size) {
+        a = new int[size];
+    }
 
     private void copy() {
         int[] newArray = new int[a.length + a.length];
@@ -11,7 +17,14 @@ public class StackImplementation {
         a = newArray;
     }
 
+    private void shrink() {
+        int[] newArray = new int[a.length / 2];
+        System.arraycopy(a, 0, newArray, 0, a.length / 2);
+        a = newArray;
+    }
+
     private void push(int value) {
+        System.out.println(pointer);
         if (pointer >= a.length) {
             copy();
         }
@@ -26,17 +39,21 @@ public class StackImplementation {
         int tempValue = a[pointer - 1];
         a[pointer - 1] = 0;
         pointer--;
+        if (pointer <= a.length / 2){
+            shrink();
+        }
         return tempValue;
     }
 
     public static void main(String[] args) {
-        StackImplementation stack = new StackImplementation();
+        StackImplementation stack = new StackImplementation(1);
         stack.push(5);
+        System.out.println(Arrays.toString(stack.a));
         stack.push(10);
+        System.out.println(Arrays.toString(stack.a));
         stack.push(15);
-        System.out.println(stack.pop());
-        System.out.println(stack.peek());
-        stack.push(100);
-        System.out.println(stack.peek());
+        System.out.println(Arrays.toString(stack.a));
+        stack.pop();
+        System.out.println(Arrays.toString(stack.a));
     }
 }
