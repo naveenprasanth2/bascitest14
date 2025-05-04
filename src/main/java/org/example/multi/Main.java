c13ef88f783b1639063eb6475a265c40b2833e2d
naveen prasanth <naveenprasanth2@gmail.com>
2025-05-04 11:12:45 +0530

    uncaught exception handler

--- src/main/java/org/example/multi/Main.java
+++ src/main/java/org/example/multi/Main.java
@@ -3,14 +3,10 @@ package org.example.multi;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         Thread thread = new Thread(() -> {
-            System.out.println(STR."We are now in thread \{Thread.currentThread().getName()}");
-            System.out.println(STR."We are now in thread with priority \{Thread.currentThread().getPriority()}");
+            throw new RuntimeException("Intentional Exception");
         });
-        thread.setName("New Worker Thread");
-        thread.setPriority(Thread.MAX_PRIORITY);
-        System.out.println(STR."We are in thread \{Thread.currentThread().getName()} before starting a new thread");
+        thread.setName("Misbehaving Thread");
+        thread.setUncaughtExceptionHandler((t, e) -> System.out.println(STR."A critical error happened in \{t.getName()} and \{e.getMessage()}"));
         thread.start();
-        System.out.println(STR."We are in thread \{Thread.currentThread().getName()} before starting a new thread");
-        Thread.sleep(2000);
     }
 }
