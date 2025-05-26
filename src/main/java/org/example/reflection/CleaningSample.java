106125639e7c510cb3bf5eaf66a2736a4f285c99
naveen prasanth <naveenprasanth2@gmail.com>
2025-05-26 10:02:10 +0530

    simplified example of cleaning example

--- src/main/java/org/example/reflection/CleaningSample.java
+++ src/main/java/org/example/reflection/CleaningSample.java
@@ -3,8 +3,10 @@ package org.example.reflection;
 import java.lang.ref.Cleaner;
 
 public class CleaningSample {
-    private final Cleaner cleaner = Cleaner.create();
-    private final Cleaner.Cleanable cleanable = cleaner.register(this, () -> System.out.println("Garbage collected"));
+
+    {
+        Cleaner.create().register(this, () -> System.out.println("Garbage collected"));
+    }
 
     public static void main(String[] args) throws InterruptedException {
         new CleaningSample();
