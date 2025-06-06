76b27055431932baeb738aed0ffd150d6382d43b
Naveen Prasanth Ramasamy <naveenprasanth12@gmail.com>
2025-06-07 05:07:03 +0530

    fixed lombok issues by removing the lombok annotations

--- src/main/java/org/example/cvent/Employee.java
+++ src/main/java/org/example/cvent/Employee.java
@@ -1,19 +1,45 @@
 package org.example.cvent;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
-@ToString
-@Getter
-@Setter
-@AllArgsConstructor
 public class Employee implements Comparable<Employee>{
 
     private int id;
     private String name;
     private int salary;
+
+    public Employee(int id, String name, int salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return STR."Employee{id=\{id}, name='\{name}\{'\''}, salary=\{salary}\{'}'}";
+    }
 
     @Override
     public int compareTo(Employee o){
