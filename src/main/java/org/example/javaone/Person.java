6d20432305b399d2187968c043feb82ac1551ae5
Naveen Prasanth Ramasamy <naveenprasanth12@gmail.com>
2025-06-07 05:04:31 +0530

    object reference returned by map impl

---
+++ src/main/java/org/example/javaone/Person.java
@@ -0,0 +1,51 @@
+package org.example.javaone;
+
+import java.util.Objects;
+
+public final class Person {
+    private int id;
+    private String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Person) obj;
+        return this.id == that.id &&
+                Objects.equals(this.name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Person[" +
+                "id=" + id + ", " +
+                "name=" + name + ']';
+    }
+
+}
